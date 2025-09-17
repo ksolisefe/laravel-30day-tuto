332bb6f57314c6b5b75783826f334625568ef222
ksolis <ksolis@efe1963.com>
2025-09-17 11:17:01 -0400

    Laravel lookin kinda cool so far

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,14 @@
     <x-slot:heading>
         Home Page
     </x-slot:heading>
-    
-    <h1>Home blade!</h1>
+
+    <h1>Welcome to the Home Page!</h1>
+    <h2>Built using the latest and greatest technologies, such as:</h2>
+    <ul>
+        <li>Laravel</li>
+        <li>PHP</li>
+        <li>HTML</li>
+        <li>Tailwind CSS</li>
+        <li>Docker soon tm</li>
+    </ul>
 </x-layout>
