f7184d93ecea540694b6a10a2ba547863fb73f36
ksolis <ksolis@efe1963.com>
2025-09-17 09:32:30 -0400

    First commit from 30day tutorial for laravael

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+</head>
+<body>
+    <h1>Thank you for reaching out to our contact page, here you can see the following: </h1>
+    <ul>
+        <li>Phone Number</li>
+        <li>Email address</li>
+        <li>P.O. Box</li>
+        <li>Alternative method of contacts</li>
+    </ul>
+</body>
+</html>
