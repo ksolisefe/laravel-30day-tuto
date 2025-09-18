8bc7aacff4f5e4847384c20a4820242b2661c059
ksolis <ksolis@efe1963.com>
2025-09-18 17:36:50 -0400

    Day 18 - Classic CRUD learning

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 // -------------------- /jobs Routes --------------------
 
-// List all jobs
+// Index - List all jobs
 Route::get('/jobs', function () {
     $jobs = Job::with(['employer', 'tags'])->latest()->simplePaginate(3);
     // $jobs = Job::all();
@@ -21,16 +21,19 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -43,6 +46,43 @@ Route::post('/jobs', function () {
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+    // authorize (On hold...)
+    
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (On hold/..)
+
+    // delete the jonb
+
+    Job::findOrFail($id)->delete();
+    
     return redirect('/jobs');
 });
 
