878145ec33b63778804595c7f74bf9d1ef8e19fb
ksolis <ksolis@efe1963.com>
2025-09-19 16:20:00 -0400

    Day 26 - Tailwind, Vite and build customization

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <title>My Website</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body class="h-full">
     <div class="min-h-full">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
